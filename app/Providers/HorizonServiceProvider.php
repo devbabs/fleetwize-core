70cfdeffc9a994cbc66e0790f69284e8a7a53385
Babalola Macaulay <babs@babs.dev>
2026-08-16 19:57:10 +0100

    chore: Give horizon view access to admin

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -30,7 +30,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         Gate::define('viewHorizon', function ($user = null) {
             return in_array(optional($user)->email, [
                 //
-            ]);
+            ]) || optional($user)->admin;
         });
     }
 }
