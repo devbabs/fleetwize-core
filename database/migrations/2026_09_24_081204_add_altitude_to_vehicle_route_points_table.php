518b162d1d6675d94bf7f7b6ac9025ab40e0acb4
Peter Adeniyi <adeniyipeteroluwatosin@gmail.com>
2026-09-24 09:51:34 +0100

    charge

--- database/migrations/2026_09_24_081204_add_altitude_to_vehicle_route_points_table.php
+++ database/migrations/2026_09_24_081204_add_altitude_to_vehicle_route_points_table.php
@@ -14,53 +14,49 @@ return new class extends Migration
         Schema::table('vehicle_route_points', function (Blueprint $table) {
             //
             Schema::table('vehicle_route_points', function (Blueprint $table) {
-                // $table->decimal('altitude', 10, 2)->nullable()->after('longitude');
+                $table->decimal('altitude', 10, 2)->nullable()->after('longitude');
 
-                // $table->decimal('accuracy', 10, 2)->nullable()->after('altitude');
+                $table->decimal('accuracy', 10, 2)->nullable()->after('altitude');
 
-                // $table->boolean('valid')->nullable()->after('accuracy');
+                $table->boolean('valid')->nullable()->after('accuracy');
 
-                // $table->boolean('blocked')->nullable();
-
-                // $table->boolean('charge')->nullable();
-
-                // $table->bigInteger('hours')->nullable();
-
-                // $table->decimal('obd_speed_kmh', 10, 2)->nullable();
-
-                // $table->decimal('obd_odometer', 15, 2)->nullable();
-
-                // $table->decimal('fuel', 10, 2)->nullable();
-
-                // $table->decimal('fuel_consumption', 10, 2)->nullable();
-
-                // $table->decimal('distance', 15, 2)->nullable();
-
-                // $table->decimal('total_distance', 20, 4)->nullable();
-
-                // $table->integer('rpm')->nullable();
-
-                // $table->decimal('engine_load', 12, 2)->nullable();
-
-                // $table->decimal('power', 10, 3)->nullable();
-
-                // $table->decimal('battery', 10, 3)->nullable();
-
-                // $table->decimal('battery_level', 10, 2)->nullable();
-
-                // $table->integer('sat')->nullable();
-
-                // $table->integer('rssi')->nullable();
-
-                // $table->integer('hard_acceleration_count')->nullable();
-
-                // $table->integer('hard_deceleration_count')->nullable();
-
-                // $table->integer('hard_cornering_count')->nullable();
+                $table->boolean('blocked')->nullable();
 
                 $table->boolean('charge')->nullable();
+
                 $table->bigInteger('hours')->nullable();
+
+                $table->decimal('obd_speed_kmh', 10, 2)->nullable();
+
+                $table->decimal('obd_odometer', 15, 2)->nullable();
+
+                $table->decimal('fuel', 10, 2)->nullable();
+
+                $table->decimal('fuel_consumption', 10, 2)->nullable();
+
+                $table->decimal('distance', 15, 2)->nullable();
+
+                $table->decimal('total_distance', 20, 4)->nullable();
+
+                $table->integer('rpm')->nullable();
+
+                $table->decimal('engine_load', 12, 2)->nullable();
+
                 $table->decimal('power', 10, 3)->nullable();
+
+                $table->decimal('battery', 10, 3)->nullable();
+
+                $table->decimal('battery_level', 10, 2)->nullable();
+
+                $table->integer('sat')->nullable();
+
+                $table->integer('rssi')->nullable();
+
+                $table->integer('hard_acceleration_count')->nullable();
+
+                $table->integer('hard_deceleration_count')->nullable();
+
+                $table->integer('hard_cornering_count')->nullable();
             });
         });
     }
